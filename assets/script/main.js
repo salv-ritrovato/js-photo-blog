2e80caf063b2d9e29b14e7d7073d3cd812c1739d
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-25 15:09:46 +0100

    reworking the project with the API i was given

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,43 @@
-/* const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+// Inizitalizing a variable where my API will go
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+
+// DOM Manipulation on the card container
+const cardContainer = document.getElementById('content')
+
+// Empty variable where our cards will be generated
+let photoCards = "";
 
 fetch(endpoint)
     .then(res => res.json())
     .then(pictures => {
         console.log(pictures);
-    }) */
+        // Cycling through the array of cards
+        for (let i = 0; i < pictures.length; i++) {
+            const cards = pictures[i];
+            // Deconstructing our array
+            const { id, title, date, url } = cards;
+            // Generating dynamic HTML for our photos
+            const dynamicHTML = `<div class="col-12 col-sm-6 col-md-6 col-lg-4 d-flex justify-content-center mb-3">
+                                    <div class="card">
+                                    <img src="./assets/img/pin.svg" alt="Pin" class="photopin">
+                                    <img src="${url}" class="card-img" alt="${title}">
+                                    <figcaption>${date}</figcaption>
+                                    <div class="card-body text-center">
+                                        <p class="card-title">Lorem ipsum dolor sit amet</p>
+                                    </div>
+                            </div>
+                        </div>
+                        `
+            // Adding our dynamic HTML to the empty variable previously inizialized
+            photoCards += dynamicHTML;
+            // The cards will be generated all at once inside the container
+            cardContainer.innerHTML = photoCards;
+        };
+    });
 
 
 
-// I'm currently working offline so I have no access to the API, working with an offline array
+/* // I'm currently working offline so I have no access to the API, working with an offline array
 const photoList = [{
     "id": 1,
     "title": "Skate Park",
@@ -45,33 +74,4 @@ const photoList = [{
     "date": "30-08-2024",
     "url": "https://marcolanci.it/boolean/assets/pictures/6.png"
 },
-];
-
-// DOM Manipulation on the card container
-const cardContainer = document.getElementById('content')
-
-// Empty variable where our cards will be generated
-let photoCards = "";
-
-// Cycling through the array of cards
-for (let i = 0; i < photoList.length; i++) {
-    const cards = photoList[i];
-    // Deconstructing our array
-    const { id, title, date, url } = cards;
-    // Generating dynamic HTML for our photos
-    const dynamicHTML = `<div class="col-12 col-sm-6 col-md-6 col-lg-4 d-flex justify-content-center mb-3">
-                            <div class="card">
-                                <img src="./assets/img/pin.svg" alt="${title}" class="photopin">
-                                <img src="./assets/img/placeholderpicture.png" class="card-img" alt="${title}">
-                                    <div class="card-body text-center">
-                                        <p class="card-title">Lorem ipsum dolor sit amet</p>
-                                    </div>
-                            </div>
-                        </div>
-                        `
-    // Adding our dynamic HTML to the empty variable previously inizialized
-    photoCards += dynamicHTML;
-};
-
-// The cards will be generated all at once inside the container
-cardContainer.innerHTML = photoCards;
+]; */
